edefb94bf7b41311777fb353ec1c55ffc2fb9abc
gabriel_rudger <gabrielrudger2003@gmail.com>
2024-04-29 21:24:15 -0300

    ComprasOnline.com 1.6.1

--- src/main/java/com/senai/ComprasOnline/Controllers/ListaProdutosController.java
+++ src/main/java/com/senai/ComprasOnline/Controllers/ListaProdutosController.java
@@ -1,10 +1,14 @@
 package com.senai.ComprasOnline.Controllers;
 
+import com.senai.ComprasOnline.DTOs.BuscarProdutoDTO;
+import com.senai.ComprasOnline.DTOs.ProdutoDTO;
 import com.senai.ComprasOnline.Services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +23,11 @@ public class ListaProdutosController {
 
         model.addAttribute("produtos", produtoService.obterListaProdutos());
         return "listaprodutos";
+    }
+
+    @PostMapping()
+    public String atualizarProduto(@ModelAttribute("buscarproduto") BuscarProdutoDTO buscarProdutoDTO){
+        return "redirect:listaprodutos";
     }
 
 
---
+++ src/main/java/com/senai/ComprasOnline/DTOs/BuscarProdutoDTO.java
@@ -0,0 +1,10 @@
+package com.senai.ComprasOnline.DTOs;
+
+import lombok.Data;
+
+@Data
+public class BuscarProdutoDTO {
+
+    private String itemBusca;
+
+}
